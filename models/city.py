224e840abf49c97f359025e4669cd96eb9897926
HassanMunene <awanzihassan@gmail.com>
2023-06-19 20:44:28 +0300

    modified the city file that defines the cities table with name and state_id column

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""
+City Module for HBNB project
+It will represent the cities table that will have two
+columns i.e name and state_id which is a foreign key
+connecting the cities and the states table
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """
+    contains name and state_id attributes
+    """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
