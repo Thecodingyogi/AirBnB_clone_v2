0fb0702128c0222d9b676931b963b7f9df5754d7
HassanMunene <awanzihassan@gmail.com>
2023-06-19 23:34:03 +0300

    updated the console to all creation of instances with clasess and their attributes

--- console.py
+++ console.py
@@ -118,13 +118,31 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args_list = args.split()
+        class_name = args_list[0]
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        params = {}
+        for param in args_list[1:]:
+            if '=' in param:
+                key, value = param.split('=')
+                value = value.replace('_', ' ')
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1].replace('\\"', '"')
+                else:
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        try:
+                            value = float(value)
+                        except ValueError:
+                            continue
+                params[key] = value
+        new_instance = HBNBCommand.classes[class_name](**params)
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
