19ea39d92c4aa55a6f3990d55616758ea3b2ddda
HassanMunene <awanzihassan@gmail.com>
2023-06-20 09:16:16 +0300

    i hope this works

--- models/base_model.py
+++ models/base_model.py
@@ -19,8 +19,8 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
@@ -42,15 +42,17 @@ class BaseModel:
         storage.save()
 
     def delete(self):
-        storage.delete(self)
+        models.storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = type(self).__name__
-        if self.created_at is not None:
-            dictionary['created_at'] = self.created_at.isoformat()
-        if self.updated_at is not None:
-            dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
-        return dictionary
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+
+        return new_dict
