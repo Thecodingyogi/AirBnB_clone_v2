86a75f5671d1d524d0d738a6885479ee3181452e
HassanMunene <awanzihassan@gmail.com>
2023-06-20 15:23:56 +0300

    declared the Review class

--- models/review.py
+++ models/review.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from models.city import City
+from models.user import User
+from os import getenv
+from models.place import Place
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """
+    Review classto store review information
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        initialize the review class
+        """
+        super().__init__(*args, **kwargs)
