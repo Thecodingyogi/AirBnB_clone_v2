4b7a064bb0904f57543083c778b68c0677578339
Thecodingyogi <cynthiatonui@gmail.com>
2023-06-20 05:56:44 -0700

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -10,24 +10,9 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    storage_type = getenv("HBNB_TYPE_STORAGE")
-
-    if storage_type == "db":
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="all, delete")
-
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
-
-    def __init__(self, *args, **kwargs):
-        """
-        initialize users
-        """
-        super().__init__(*args, **kwargs)
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
