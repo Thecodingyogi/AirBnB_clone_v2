c71991a9e9ddcf0f90fedaf2e4fb679c21414cf3
Thecodingyogi <cynthiatonui@gmail.com>
2023-07-26 02:23:20 -0700

    updated

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,25 +2,26 @@
 """script that starts a Flask web application"""
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_session(exception):
-    """Removes the current SQLAlchemy session after each request"""
-    storage.close()
-
-
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    """Displays the main hbnb filters HTML page"""
+    """Displays the main HBNB filters HTML page."""
     states = storage.all(State)
-    amenities = storage.all("Amenity")
+    amenities = storage.all(Amenity)
     return render_template("10-hbnb_filters.html", states=states,
                            amenities=amenities)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.teardown_appcontext
+def teardown(exception):
+    """Removes the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
